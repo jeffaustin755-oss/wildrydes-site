13aedaba76f41a5d6f4c772db90720a8165ed88b
jeffaustin755-oss <jeffaustin755@gmail.com>
2025-11-13 22:06:20 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_s7kpnDfu7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '30qtcn6ieia3ngufrek2qcslrl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'qc0n9gmf53g734j7q6cpb4ndd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
